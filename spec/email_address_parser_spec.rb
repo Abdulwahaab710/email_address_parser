3d7d8e1fc66168fb0ec3cb6e17d91c397c29cac1
Abdulwahaab Ahmed <abdulwahaab.ahmed710@gmail.com>
2018-05-07 00:14:27 -0400

    removing unwanted test

--- spec/email_address_parser_spec.rb
+++ spec/email_address_parser_spec.rb
@@ -2,8 +2,4 @@ RSpec.describe EmailAddressParser do
   it "has a version number" do
     expect(EmailAddressParser::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
