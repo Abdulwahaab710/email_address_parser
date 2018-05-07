ee31aef3fab615972ca0065de26b9d5aa490a883
Abdulwahaab Ahmed <abdulwahaab.ahmed710@gmail.com>
2018-05-07 00:14:12 -0400

    moved the class to inside a module

--- lib/email_address_parser/parser.rb
+++ lib/email_address_parser/parser.rb
@@ -1,33 +1,35 @@
-class Parser
-  attr_reader :email_address
+module EmailAddressParser
+  class Parser
+    attr_reader :email_address
 
-  def initialize(email_address)
-    @email_address = email_address
+    def initialize(email_address)
+      @email_address = email_address
+    end
+
+    def local_part
+      @email_address[/^([A-Za-z0-9\.]+)/,1]
+    end
+
+    def tag
+      @email_address[/\+([A-Za-z0-9]+)@/,1]
+    end
+
+    def domain_name
+      @email_address[/@([A-Za-z\.]+)/,1]
+    end
+
+    def top_level_domain
+      @email_address[/\.([A-Za-z]+)$/, 1]
+    end
+
+    def tag?
+      !tag.nil?
+    end
+
+    def valid?
+      raise NotImplementedError
+    end
+
+    alias_method :username, :local_part
   end
-
-  def local_part
-    @email_address[/^([A-Za-z0-9\.]+)/,1]
-  end
-
-  def tag
-    @email_address[/\+([A-Za-z0-9]+)@/,1]
-  end
-
-  def domain_name
-    @email_address[/@([A-Za-z\.]+)/,1]
-  end
-
-  def top_level_domain
-    @email_address[/\.([A-Za-z]+)$/, 1]
-  end
-
-  def tag?
-    !tag.nil?
-  end
-
-  def valid?
-    raise NotImplementedError
-  end
-
-  alias_method :username, :local_part
 end
